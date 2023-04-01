c201fabd4ed578fdcfd6e4dccd6ab5df0bbe32d0
Rishikpamuru <rishik.pamuru@gmail.com>
2023-03-31 18:28:06 -0600

    commit added cal and stuff

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask,request, render_template
 
 app = Flask(__name__)
+
+
+{
+
+}
 
 @app.route("/")
 def index():
@@ -9,3 +14,15 @@ def index():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/cal", methods=["GET", "POST"])
+def cal():
+    ans = 0
+    x = request.form.get("x")
+    y = request.form.get("y")
+
+    if x is not None and y is not None:
+        ans = int(x) + int(y)
+
+
+    return render_template("cal.html", ans=ans)
