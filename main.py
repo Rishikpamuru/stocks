1fe648a6abbfd881f12afd875314f20f2f138909
Rishikpamuru <rishik.pamuru@gmail.com>
2023-04-12 17:51:49 -0600

    added login

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-from flask import Flask,request, render_template
+from flask import Flask, request, render_template, redirect, session, url_for
 
 app = Flask(__name__)
-
-
-{
-
-}
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login",methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+
+        return redirect(url_for("index"))
+    
     return render_template("login.html")
 
-@app.route("/cal", methods=["GET", "POST"])
-def cal():
-    ans = 0
-    x = request.form.get("x")
-    y = request.form.get("y")
-
-    if x is not None and y is not None:
-        ans = int(x) + int(y)
-
-
-    return render_template("cal.html", ans=ans)
